8a06e3bf21a6066c8de48be40aa5fbf78c8c888d
Luiz Gustavo Dos Santos Pereira <luizgustavosantospreira@gmail.com>
2025-03-04 02:18:56 -0300

    Add files via upload

---
+++ Carta/script.js
@@ -0,0 +1,10 @@
+const papel = document.getElementById("iloveyou")
+const carta = document.getElementById("carta")
+const abrirr = document.getElementById("abrir")
+
+
+function abrir() {
+        papel.style.animationPlayState = "running"
+        carta.style.animationPlayState = "running"
+        abrirr.style.animationPlayState = "running"
+}
